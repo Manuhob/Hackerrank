340186aca196fd57e729b3f1c87bb739d3afcbb0
Manuhob <sedanom@gmail.com>
2023-05-11 14:53:08 -0600

    formating testing section on container ballsOF

--- containersBalls.py
+++ containersBalls.py
@@ -8,6 +8,7 @@
     #if after sorting the above lists, we have a coincidence, then return possible, otherwise impossible    
 def organizingContainers(container):
     ballsperContainer = [] #amount of balls in each container
+    n = len(container[0])
     ballsof_eachtype = [0 for x in range(n)]    #amount of balls of each type
     for inform in container:
         total = 0
@@ -24,9 +25,11 @@ def organizingContainers(container):
 if __name__ == '__main__':
 
     q = 2 #number of queries
-    n = 3 #number of containers (first querie)
-    container = [[1,3,1],[2,1,2],[3,3,3]] #containers
-    print(organizingContainers(container) + '\n')
-    n = 3 #number of containers (second querie)
-    container = [[0,2,1],[1,1,1],[2,0,0]] #containers
-    print(organizingContainers(container) + '\n')
+    test_cases = [[[1,3,1],[2,1,2],[3,3,3]], [[0,2,1],[1,1,1],[2,0,0]]]
+    output =['Impossible', 'Possible']
+    for i in range(q):
+        if organizingContainers(test_cases[i]) == output[i]:
+            print(f'Test case {i+1}: Success!!')
+        else:
+            print(f'Test case {i+1}: Failed!!')
+

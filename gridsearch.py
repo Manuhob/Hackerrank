579b099c0930d238f0f52718db785207252cfa9e
Manuhob <sedanom@gmail.com>
2023-05-14 20:04:59 -0600

    first edition to gridsearch

--- gridsearch.py
+++ gridsearch.py
@@ -17,40 +17,51 @@ import sys
 
 def gridSearch(G, P):
     # Write your code here
+    pass
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+def stringtoarray(s):
+    result = []
+    for x in s:
+        result.append(x)
+    return result
 
-    t = int(input().strip())
 
-    for t_itr in range(t):
-        first_multiple_input = input().rstrip().split()
+#Test case 1
+s = '7283455864 6731158619 8988242643 3830589324 2229505813 5633845374 6473530293 7053106601 0834282956 4607924137'
+G = []
+for x in s.split(' '):
+    G.append(stringtoarray(x))
+s = '9505 3845 3530'
+P = []
+for x in s.split(' '):
+    P.append(stringtoarray(x))
 
-        R = int(first_multiple_input[0])
+print(gridSearch(G, P))
 
-        C = int(first_multiple_input[1])
+#Test case n
+s = '400453592126560 114213133098692 474386082879648 522356951189169 887109450487496 252802633388782 502771484966748 075975207693780 511799789562806 404007454272504 549043809916080 962410809534811 445893523733475 768705303214174 650629270887160'
+G = []
+for x in s.split(' '):
+    G.append(stringtoarray(x))
+s = '99 99'
+P = []
+for x in s.split(' '):
+    P.append(stringtoarray(x))
 
-        G = []
+print(gridSearch(G, P))
 
-        for _ in range(R):
-            G_item = input()
-            G.append(G_item)
 
-        second_multiple_input = input().rstrip().split()
 
-        r = int(second_multiple_input[0])
 
-        c = int(second_multiple_input[1])
 
-        P = []
 
-        for _ in range(r):
-            P_item = input()
-            P.append(P_item)
 
-        result = gridSearch(G, P)
 
-        fptr.write(result + '\n')
 
-    fptr.close()
+
+
+
+
+
+
 
